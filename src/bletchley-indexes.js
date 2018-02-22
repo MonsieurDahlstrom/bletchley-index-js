87083028c8e180d9eea8672d44bdac08de67500c
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2018-02-22 17:43:35 +0800

    updated test specs for month detecting

--- src/bletchley-indexes.js
+++ src/bletchley-indexes.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import parse from 'csv-parse/lib/sync'
+import moment from 'moment'
+import { extendMoment } from 'moment-range';
+
+extendMoment(moment)
 
 import BletchleyIndex from './bletchley-index'
 import BletchleyIndexMember from './bletchley-index-member'
@@ -13,8 +17,7 @@ class BletchleyIndexContainer {
   }
 
   async retriveIndexes(date) {
-    let locale = 'en-US'
-    let month = date.toLocaleString(locale, { month: "short" }).toLowerCase()
+    let month = this.retriveMonthForDate(date)
     let url = "https://www.bletchleyindexes.com/weights/" + month + ".csv"
     let result = await axios.get(url)
     if(result.status === 200) await this.parseIndexData(result.data,date)
@@ -22,6 +25,71 @@ class BletchleyIndexContainer {
     return list
   }
 
+  retriveMonthForDate(date) {
+    let result = null
+    let locale = 'en-US'
+    //
+    let startOfMonth = moment().startOf("month")
+    let endOfMonth = moment().endOf("month")
+    let thisMonthRange = moment.range(startOfMonth,this.thirdWednesdayOfMonth(date))
+    let previousMonthsThisYear = moment.range(moment().startOf("year"), startOfMonth)
+    let lastYearMonths = moment.range(moment().subtract(12,"month").startOf("month"), moment().startOf("year"))
+    if(lastYearMonths.contains(date)) {
+      result = date.toLocaleString(locale, { month: "short" }).toLowerCase()
+    } else if(previousMonthsThisYear.contains(date)) {
+      result = date.toLocaleString(locale, { month: "short" }).toLowerCase()
+    } else if( thisMonthRange.contains(date) ) {
+      result = date.toLocaleString(locale, { month: "short" }).toLowerCase()
+    }
+    return result
+  }
+
+  thirdWednesdayOfMonth(date) {
+    let startOfMonth = moment(date).startOf("month").startOf('isoweek');
+    let threeWeeksInMonth = moment(startOfMonth).add(3,"w")
+    let thirdThursday = moment(threeWeeksInMonth).add(2, 'd');
+    return startOfMonth.month() === thirdThursday.month() ? moment(thirdThursday).subtract(1,"w") : thirdThursday
+  }
+
+  /*
+  retriveMonthAbbrivation(date) {
+    let dateNow = moment()
+    let desiredDate = moment(date)
+    let locale = 'en-US'
+    let abbrivation = null
+    if(desiredDate > dateNow) {
+      throw Error("BletchleyIndexes.retriveMonthAbbrivation future date provided")
+    }
+    if(desiredDate.year() === dateNow.year()) {
+      if( desiredDate.month() > this.getThirdThursday(dateNow) ) {
+        throw Error("BletchleyIndexes.retriveMonthAbbrivation future date provided")
+      }
+    }
+    let oneYearAgo = moment(dateNow).subtract(1,"year")
+    if( desiredDate.year() === oneYearAgo.year() ) {
+      if( desiredDate.month() < dateNow.month() ) {
+        throw Error("BletchleyIndexes.retriveMonthAbbrivation date provided is to old")
+      }
+      else if(desiredDate.month() === dateNow.month() && dateNow >= this.getThirdThursday(dateNow) ) {
+        throw Error("BletchleyIndexes.retriveMonthAbbrivation date provided is to old")
+      }
+    }
+    if (desiredDate.year() < oneYearAgo.year() ) {
+      throw Error("BletchleyIndexes.retriveMonthAbbrivation date provided is to old")
+    }
+    abbrivation = desiredDate.toDate().toLocaleString(locale, { month: "short" }).toLowerCase()
+    return abbrivation
+  }
+
+  getThirdThursday(startDate) {
+    var startOfMonth = moment(startDate).startOf('month').startOf('isoweek');
+    var thirdThursday = moment(startDate).startOf('month').startOf('isoweek').add(3, 'w').add(4, 'd');
+    if (thirdThursday.month() == startOfMonth.month()) {
+      thirdThursday.subtract(1, 'w');
+    }
+    return thirdThursday.startOf("day");
+  }
+*/
   async parseIndexData(data,date) {
     let rows = parse(data,{})
     for(let row of rows) {
--- tests/unit/bletchley-indexes/download.spec.js
+++ tests/unit/bletchley-indexes/download.spec.js
@@ -1,8 +1,12 @@
 //
 import 'babel-polyfill'
 //
-import {expect} from 'chai'
+import chaiAsPromised from "chai-as-promised";
+import chai,{expect} from 'chai'
 import sinon from 'sinon';
+
+chai.use(chaiAsPromised);
+
 //
 import moment from 'moment'
 import fs from 'fs';
@@ -15,6 +19,60 @@ import BletchleyIndexes from '../../../src/index'
 
 describe('BletchleyIndexes', function() {
 
+  describe.only("#thirdWednesdayOfMonth", function () {
+    let day
+    beforeEach(function() {
+      day = Math.floor(Math.random() * (28 - 1 + 1)) + 1;
+    })
+    it("September 2028", function () {
+      let date = moment("2028-09-" + day, "YYYY-MM-DD").toDate()
+      let result = BletchleyIndexes.thirdWednesdayOfMonth(date).format("YYYY-MM-DD")
+      expect(result).to.equal("2028-09-20")
+    })
+    it("April 2018", function() {
+      let date = moment("2018-04-" + day, "YYYY-MM-DD").toDate()
+      let result = BletchleyIndexes.thirdWednesdayOfMonth(date).format("YYYY-MM-DD")
+      expect(result).to.equal("2018-04-18")
+    })
+    it("October 2017", function () {
+      let date = moment("2017-10-" + day, "YYYY-MM-DD").toDate()
+      let result = BletchleyIndexes.thirdWednesdayOfMonth(date).format("YYYY-MM-DD")
+      expect(result).to.equal("2017-10-18")
+    })
+    it("January 1996", function() {
+      let date = moment("1996-01-" + day, "YYYY-MM-DD").toDate()
+      let result = BletchleyIndexes.thirdWednesdayOfMonth(date).format("YYYY-MM-DD")
+      expect(result).to.equal("1996-01-17")
+    })
+    it("July 1945", function () {
+      let date = moment("1945-07-" + day, "YYYY-MM-DD").toDate()
+      let result = BletchleyIndexes.thirdWednesdayOfMonth(date).format("YYYY-MM-DD")
+      expect(result).to.equal("1945-07-18")
+    })
+  })
+
+  describe.only("#retriveMonthForDate", function () {
+    let clock
+    afterEach(function() {
+      if(clock)
+        clock.restore();
+    })
+    it("wont return month for dates older then 13 months", function () {
+      let yearAgo = moment().subtract(13,"month").toDate()
+      expect(BletchleyIndexes.retriveMonthForDate(yearAgo)).to.equal(null)
+    })
+    it("return month 12 months ago if bletchley index has not been refreshed", function () {
+      let date = moment().subtract(12,"months").startOf("month").toDate()
+      let bletchleyIndexDate = BletchleyIndexes.thirdWednesdayOfMonth(new Date())
+      let clock = sinon.useFakeTimers();
+      clock.tick( bletchleyIndexDate.subtract(1,"day").toDate().getTime() )
+      expect(BletchleyIndexes.retriveMonthForDate(date)).to.be.an("string")
+    })
+    it("wont month 12 months ago if bletchley index has been reached", function () {
+      let date = moment().subtract(12,"m").endOf("month").toDate()
+      expect(BletchleyIndexes.retriveMonthForDate(date)).to.equal(null)
+    })
+  })
   describe('#retriveIndexes()', function() {
 
     beforeEach(function() {
@@ -25,11 +83,54 @@ describe('BletchleyIndexes', function() {
       this.mock.restore();
     })
 
+    describe("different dates", function() {
+        let clock
+        let now
+        let futureDate
+        let monthsAgo13
+        let monthsAgo12Start
+        let monthsAgo12End
+        beforeEach(function () {
+          now = new Date()
+          futureDate = moment().add(1,'month').toDate()
+          monthsAgo13 = moment().subtract(1,'year').subtract(1,"month").toDate()
+          monthsAgo12Start = moment().subtract(1,'year').startOf('month').toDate()
+          monthsAgo12End = moment().subtract(1,'year').endOf('month').toDate()
+          clock = sinon.useFakeTimers();
+          let datapath = path.resolve(__dirname, '../../data/jan.csv')
+          let csvfile = fs.readFileSync(datapath, "utf8");
+          this.mock.onGet(/https:\/\/www.bletchleyindexes.com\/weights\/[a-zA-Z]{3}.csv/).reply(200,csvfile)
+
+        })
+        afterEach(function () {
+          clock.restore()
+        })
+        it("should fail for future date", async () => {
+          clock.tick( now.getTime() )
+          return expect(BletchleyIndexes.retriveIndexes(futureDate)).to.eventually.be.rejectedWith("BletchleyIndexes.retriveMonthAbbrivation future date provided");
+        })
+        it('13 months ago fails', async () => {
+          clock.tick( now.getTime() )
+          return expect(BletchleyIndexes.retriveIndexes(monthsAgo13)).to.eventually.be.rejectedWith("BletchleyIndexes.retriveMonthAbbrivation date provided is to old");
+        }),
+        it('12 months ago before bletchley index date', async () => {
+          clock.tick( moment(now).startOf("month").toDate().getTime() )
+          return expect(BletchleyIndexes.retriveIndexes(monthsAgo12Start)).to.eventually.be.fulfilled;
+        })
+        it('12 months ago after bletchley index date', async () =>{
+          clock.tick( moment(now).endOf("month").toDate().getTime() )
+          return expect(BletchleyIndexes.retriveIndexes(monthsAgo12End)).to.eventually.be.rejectedWith("BletchleyIndexes.retriveMonthAbbrivation date provided is to old");
+        })
+        it('this month before third Wednesday')
+        it('this month after third Wednesday')
+        it('a month in the last year')
+    })
+
     describe('network request succeed', function() {
       beforeEach(function() {
         let datapath = path.resolve(__dirname, '../../data/jan.csv')
         let csvfile = fs.readFileSync(datapath, "utf8");
-        this.mock.onGet('https://www.bletchleyindexes.com/weights/jan.csv').reply(200,csvfile)
+        this.mock.onGet(/https:\/\/www.bletchleyindexes.com\/weights\/[a-zA-Z]{3}.csv/).reply(200,csvfile)
       })
       it('parse results', function(done) {
         BletchleyIndexes.retriveIndexes(new Date(2018, 0 ,1))
@@ -55,7 +156,7 @@ describe('BletchleyIndexes', function() {
 
     describe('network request failed', function() {
       beforeEach(function() {
-        this.mock.onGet('https://www.bletchleyindexes.com/weights/jan.csv').networkError();
+        this.mock.onGet(/https:\/\/www.bletchleyindexes.com\/weights\/[a-zA-Z]{3}.csv/).networkError()
       })
       it('parse results', function(done) {
         let date = moment().add(1, 'month')
